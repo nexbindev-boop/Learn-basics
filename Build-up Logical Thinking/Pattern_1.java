0453e354b54621fcd46b3706d0f90ba961bd840b
Himanshu <himanshupatel6895@gmail.com>
2026-06-09 23:49:19 +0530

    Initial commit

--- Build-up Logical Thinking/Pattern_1.java
+++ Build-up Logical Thinking/Pattern_1.java
@@ -2,5 +2,6 @@ public class Pattern_1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hiii");
+        System.out.println("hiii");
     }
 }
